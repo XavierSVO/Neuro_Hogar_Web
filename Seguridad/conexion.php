626de596e8eb4ea90a9460e644f663c034ab9c54
BRICIOJC <fabriciojimenezcuellar@gmail.com>
2020-11-06 15:38:48 -0400

    Update conexion.php
    Actualización conexión

--- Seguridad/conexion.php
+++ Seguridad/conexion.php
@@ -1,7 +1,7 @@
 <?php
 $server = "http://142.44.247.98/";
-$user = "root";
-$pass = ""; 
+$user = "mega";
+$pass = "xE#0rXgMqmEQD^$BIP$odf"; 
 $db= "neuro_hogar";
 
 $conexion = mysqli_connect($server, $user, $pass,$db);	
