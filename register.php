5bc050caea2ec91560319b48cacc27d8f132f0f1
Xavier Vaca <xaviersebastian2000@gmail.com>
2020-11-20 13:31:15 -0400

    Editando conexion de la base de datos con el archivo de registres

--- register.php
+++ register.php
@@ -2,7 +2,7 @@
 
 
 //momento de conectarnos a db
-$conn = mysqli_connect("localhost","admin_cursoiot","121212","admin_cursoiot");
+$conn = mysqli_connect("localhost","chuturubi","m@squiTt-m@sc@IOTchuturubi1923","chuturubi");
 
 if ($conn==false){
   echo "Hubo un problema al conectarse a María DB";
