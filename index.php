c11ae5d9119718474fbbeb5e90e8847f29db6ad9
Xavier Vaca <xaviersebastian2000@gmail.com>
2020-11-27 12:06:42 -0400

    probando por mqtt://

--- index.php
+++ index.php
@@ -199,7 +199,7 @@ const options = {
 // mqtts Encrypted TCP connection
 // wxs WeChat applet connection
 // alis Alipay applet connection
-const connectUrl = '142.44.247.98:1883'
+const connectUrl = 'mqtt://142.44.247.98:1883'
 const client = mqtt.connect(connectUrl, options)
 
 client.on('reconnect', (error) => {
