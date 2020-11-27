a674edd581e6ef50678e7e25b758c984bb2d0b03
Xavier Vaca <xaviersebastian2000@gmail.com>
2020-11-27 12:17:57 -0400

    direccion ip 0.0.0.0

--- index.php
+++ index.php
@@ -199,7 +199,7 @@ const options = {
 // mqtts Encrypted TCP connection
 // wxs WeChat applet connection
 // alis Alipay applet connection
-const connectUrl = 'wss://142.44.247.98:6001'
+const connectUrl = 'wss://0.0.0.0:6001'
 const client = mqtt.connect(connectUrl, options)
 
 client.on('reconnect', (error) => {
