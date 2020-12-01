e1c514e71a5cf7945da7917c0b5151ee26527951
Xavier Vaca <xaviersebastian2000@gmail.com>
2020-12-01 16:43:16 -0400

    usando puerto 1883

--- index.php
+++ index.php
@@ -199,7 +199,7 @@ const options = {
 // mqtts Encrypted TCP connection
 // wxs WeChat applet connection
 // alis Alipay applet connection
-const connectUrl = 'wss://142.44.247.98:8093/tcp'
+const connectUrl = 'mqtt://142.44.247.98:1883'
 const client = mqtt.connect(connectUrl, options)
 
 client.on('reconnect', (error) => {
