f42c49e97937e1634421db5755010582dd4ecb8d
Xavier Vaca <xaviersebastian2000@gmail.com>
2020-12-05 23:49:25 -0400

    Cambiando el nombre chuturubi en el index

--- index.php
+++ index.php
@@ -74,7 +74,7 @@ if (!$db_selected) {
 <html lang="en">
 <head>
   <meta charset="utf-8" />
-  <title>IoT Masterclass</title>
+  <title>Chuturubi</title>
   <meta name="description" content="Admin, Dashboard, Bootstrap, Bootstrap 4, Angular, AngularJS" />
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimal-ui" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
