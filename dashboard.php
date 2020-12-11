bdcdc368008afdaeb3f1bdb474e5cb7557d9aaae
Xavier Vaca <xaviersebastian2000@gmail.com>
2020-12-11 11:47:44 -0400

    Colocando el valor del led

--- dashboard.php
+++ dashboard.php
@@ -645,6 +645,7 @@ client.on('connect', function () {
 client.on('message', function (topic, message) {
   // message is Buffer
   console.log(message.toString())
+  $('#display_led').html(message.toString())
 
 })
 
