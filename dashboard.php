d1c85a82b0f09bf21b49a3489d85eb7698ee80bf
Xavier Vaca <xaviersebastian2000@gmail.com>
2020-12-15 10:13:52 -0400

    Poniendo la funcion en el switch

--- dashboard.php
+++ dashboard.php
@@ -259,8 +259,7 @@ $matches = implode(',', $array);
                   <small class="text-muted">Promedio: 17 C</small>
                 </div>
                 <div>
-                <input type="checkbox" id="switch-led">
-                <b onchange="process_led()" id="display_led">--</b>
+                <input onchange="process_led()" type="checkbox" id="switch-led">
                 </div>
               </div>
             </div>
